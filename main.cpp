3a67d01ac88b97c06bca63491c9c10fd9b0603f6
robigan <35210888+robigan@users.noreply.github.com>
2022-12-27 07:11:41 +1100

    Write more DRY and concise code

--- main.cpp
+++ main.cpp
@@ -1,79 +1,55 @@
 #include <iostream>
 
-#define BOARD_ROWS 6
-#define BOARD_COLUMNS 6
+#define BOARD_ROWS 4
+#define BOARD_COLUMNS 4
 
 using namespace std;
 
-void populate_board(int (&board)[10][10]) {
-
-}
-
-void draw_row(int (&board)[BOARD_ROWS][BOARD_COLUMNS], const int &i, const int &j1, int count, bool reverse = false) {
-    for (int k = 3 - to_string(count).length(); k > 0; k--) { // Print initial spacing if needed
-    // Could be optimized using maths but a simple to_string() call is easier
-        cout << " ";
-    }
-
-    if (!reverse) {
-        for (int j = 0; j < j1; j++) {
-            board[i][j] = count;
-            count--;
-
-            cout << board[i][j];
-            
-            if (j < j1 - 1) {
-                cout << " || ";
-            }
-
-            for (int k = 3 - to_string(count).length(); k > 0; k--) {
-                cout << " ";
-            }
-        }
-    } else {
-        for (int j = j1; j > 0; j--) {
-            board[i][j] = count; // Populate board
-            count++;
-
-            cout << board[i][j]; // Print number
-
-            if (j > 1) { // Check to not print the column divider at the end
-                cout << " || ";
-            }
-
-            for (int k = 3 - to_string(count).length(); k > 0; k--) { // Print spacing based on chracter length
-                cout << " ";
-            }
-        }
-    }
-}
-
-void draw_board(int (&board)[BOARD_ROWS][BOARD_COLUMNS], int i1, int j1) {
+void populate_board(int (&board)[BOARD_ROWS][BOARD_COLUMNS]) {
     int count = BOARD_ROWS * BOARD_COLUMNS;
 
-    int i2 = i1 / 2; // Half the board count to account also for reversed board rows
+    for (int i = 0; i < BOARD_ROWS; i++) {
+        for (int j = 0; j < BOARD_COLUMNS; j++) {
+            board[i][j] = count;
+            count--;
+        }
 
-    for (int i = 0; i < i2; i++) {
-        draw_row(board, i, j1, count);
-        count -= j1;
-        cout << endl;
+        i++; // Manually increase i by 1
+        for (int j = BOARD_COLUMNS - 1; j > -1; j--) { // Count down in reverse
+            board[i][j] = count;
+            count--;
+        }
+    }
+}
 
-        count -= j1;
-        draw_row(board, i, j1, count + 1, true);
+void draw_row(int (&board)[BOARD_ROWS][BOARD_COLUMNS], const int &i) {
+    for (int j = 0; j < BOARD_COLUMNS; j++) {
+        for (int k = 3 - to_string(board[i][j]).length(); k > 0; k--) {
+            cout << " ";
+        }
+
+        cout << board[i][j];
+        
+        if (j < BOARD_COLUMNS - 1) {
+            cout << " || ";
+        }
+    }
+}
+
+void draw_board(int (&board)[BOARD_ROWS][BOARD_COLUMNS]) {
+    for (int i = 0; i < BOARD_ROWS; i++) {
+        draw_row(board, i);
         cout << endl;
     }
 
-    // if (i2 % 2 != 0) {
-    //     count -= j1;
-    //     draw_row(board, i, j1, count + 1, true);
-    //     cout << endl;
-    // }
+    cout << "Finished!" << endl;
 }
 
 int main() {
     int board[BOARD_ROWS][BOARD_COLUMNS];
 
-    draw_board(board, BOARD_ROWS, BOARD_COLUMNS);
+    populate_board(board);
+    draw_board(board);
 
     return 0;
 }
